7c529b931bc444e67a96c1b43c873335114626e2
KHkikani <113745196+KHkikani@users.noreply.github.com>
2022-11-16 13:42:08 +0530

    Update cartPage.dart

--- lib/Screens/cartPage.dart
+++ lib/Screens/cartPage.dart
@@ -16,11 +16,18 @@ class _cartPageState extends State<cartPage> {
   void initState() {
     super.initState();
 
-    cartItem = cartItem.toSet().toList();
+    cartItem =  cartItem.toSet().toList();
   }
+
+  late int totalAmount;
 
   @override
   Widget build(BuildContext context) {
+    totalAmount = 0;
+    cartItem.forEach((element) {
+      totalAmount += element.productPrize * element.selectedItem;
+    });
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Cart"),
@@ -29,177 +36,204 @@ class _cartPageState extends State<cartPage> {
       ),
       body: SingleChildScrollView(
         physics: BouncingScrollPhysics(),
-        child: Column(
-          children: cartItem
+        child: Column(children: [
+          ...cartItem
               .map<Widget>((e) => GestureDetector(
-            onTap: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) =>
-                      ProductDetailsPage(
-                        product: e,
-                      ),
-                ),
-              );
-            },
-                child: Container(
-                    margin: EdgeInsets.all(5),
-                    padding: EdgeInsets.all(5),
-                    decoration: BoxDecoration(
-                      color: Colors.blueGrey.withOpacity(0.5),
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(25),
-                      ),
-                    ),
-
-                    // height: 300,
-                    width: double.infinity,
-                    child: Row(
-                      children: [
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              Container(
-                                height: 200,
-                                width: 175,
-                                clipBehavior: Clip.antiAlias,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.all(
-                                    Radius.circular(25),
-                                  ),
-                                ),
-                                child: Image.network(
-                                  e.productImage,
-                                  width: 200,
-                                  height: 200,
-                                  fit: BoxFit.fitHeight,
-                                ),
-                              ),
-                              SizedBox(
-                                height: 5,
-                              ),
-                              Text(
-                                "${e.productPrize} RS",
-                                style: TextStyle(
-                                    fontSize: 22, fontWeight: FontWeight.bold),
-                              ),
-                              SizedBox(
-                                height: 5,
-                              ),
-                              Text("${e.productName}",
-                                  style: TextStyle(
-                                      fontSize: 16, fontWeight: FontWeight.w500),
-                                  textAlign: TextAlign.center),
-                              SizedBox(
-                                height: 5,
-                              ),
-                              SizedBox(
-                                height: 5,
-                              ),
-                            ],
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => ProductDetailsPage(
+                            product: e,
                           ),
                         ),
-                        Expanded(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              TextButton(
-                                  onPressed: () {
-                                    showDialog(
-                                        context: context,
-                                        builder: (context) => Dialog(
-                                              shape: RoundedRectangleBorder(
-                                                  borderRadius: BorderRadius.all(
-                                                      Radius.circular(25))),
-                                              clipBehavior: Clip.antiAlias,
-                                              child: Container(
-                                                height: 300,
-                                                decoration: BoxDecoration(
-                                                    borderRadius:
-                                                        BorderRadius.all(
-                                                            Radius.circular(25))),
-                                                child: SingleChildScrollView(
-                                                  child: Column(
-                                                    children: e.productDescription
-                                                        .map<Widget>((e) =>
-                                                            ListTile(
-                                                              title:
-                                                                  Text("${e[0]}"),
-                                                              subtitle:
-                                                                  Text("${e[1]}"),
-                                                            ))
-                                                        .toList(),
-                                                  ),
-                                                ),
-                                              ),
-                                            ));
-                                  },
-                                  child: Text("View Description")),
-                              Text(
-                                "${e.selectedItem * e.productPrize}",
-                                style: TextStyle(
-                                    fontSize: 25, fontWeight: FontWeight.w500),
-                              ),
-                              SizedBox(
-                                height: 5,
-                              ),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      );
+                    },
+                    child: Container(
+                        margin: EdgeInsets.all(5),
+                        padding: EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                          color: Colors.blueGrey.withOpacity(0.5),
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(25),
+                          ),
+                        ),
+
+                        // height: 300,
+                        width: double.infinity,
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
                                 children: [
-                                  IconButton(
-                                    onPressed: () {
-                                      setState(() {
-                                        if (e.selectedItem > 1) {
-                                          e.selectedItem -= 1;
-                                        }
-                                      });
-                                    },
-                                    icon: Icon(Icons.remove, color: Colors.red),
+                                  Container(
+                                    height: 200,
+                                    width: 175,
+                                    clipBehavior: Clip.antiAlias,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.all(
+                                        Radius.circular(25),
+                                      ),
+                                    ),
+                                    child: Image.network(
+                                      e.productImage,
+                                      width: 200,
+                                      height: 200,
+                                      fit: BoxFit.fitHeight,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 5,
                                   ),
                                   Text(
-                                    "${e.selectedItem.toString()}",
+                                    "${e.productPrize} RS",
+                                    style: TextStyle(
+                                        fontSize: 22,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  SizedBox(
+                                    height: 5,
+                                  ),
+                                  Text("${e.productName}",
+                                      style: TextStyle(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.w500),
+                                      textAlign: TextAlign.center),
+                                  SizedBox(
+                                    height: 5,
+                                  ),
+                                  SizedBox(
+                                    height: 5,
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Expanded(
+                              child: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  TextButton(
+                                      onPressed: () {
+                                        showDialog(
+                                            context: context,
+                                            builder: (context) => Dialog(
+                                                  shape: RoundedRectangleBorder(
+                                                      borderRadius:
+                                                          BorderRadius.all(
+                                                              Radius.circular(
+                                                                  25))),
+                                                  clipBehavior: Clip.antiAlias,
+                                                  child: Container(
+                                                    height: 300,
+                                                    decoration: BoxDecoration(
+                                                        borderRadius:
+                                                            BorderRadius.all(
+                                                                Radius.circular(
+                                                                    25))),
+                                                    child:
+                                                        SingleChildScrollView(
+                                                      child: Column(
+                                                        children: e
+                                                            .productDescription
+                                                            .map<Widget>(
+                                                                (e) => ListTile(
+                                                                      title: Text(
+                                                                          "${e[0]}"),
+                                                                      subtitle:
+                                                                          Text(
+                                                                              "${e[1]}"),
+                                                                    ))
+                                                            .toList(),
+                                                      ),
+                                                    ),
+                                                  ),
+                                                ));
+                                      },
+                                      child: Text("View Description")),
+                                  Text(
+                                    "${e.selectedItem * e.productPrize}",
                                     style: TextStyle(
                                         fontSize: 25,
                                         fontWeight: FontWeight.w500),
                                   ),
-                                  IconButton(
+                                  SizedBox(
+                                    height: 5,
+                                  ),
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceAround,
+                                    children: [
+                                      IconButton(
+                                        onPressed: () {
+                                          setState(() {
+                                            if (e.selectedItem > 1) {
+                                              e.selectedItem -= 1;
+                                            }
+                                          });
+                                        },
+                                        icon: Icon(Icons.remove,
+                                            color: Colors.red),
+                                      ),
+                                      Text(
+                                        "${e.selectedItem.toString()}",
+                                        style: TextStyle(
+                                            fontSize: 25,
+                                            fontWeight: FontWeight.w500),
+                                      ),
+                                      IconButton(
+                                        onPressed: () {
+                                          setState(() {
+                                            e.selectedItem += 1;
+                                          });
+                                        },
+                                        icon: Icon(Icons.add,
+                                            color: Colors.green),
+                                      ),
+                                    ],
+                                  ),
+                                  SizedBox(
+                                    height: 5,
+                                  ),
+                                  ElevatedButton(
+                                    style: ButtonStyle(
+                                        backgroundColor:
+                                            MaterialStateProperty.all(
+                                                Colors.green)),
+                                    onPressed: () {},
+                                    child: Text("Buy"),
+                                  ),
+                                  ElevatedButton(
                                     onPressed: () {
                                       setState(() {
-                                        e.selectedItem += 1;
+                                        cartItem.remove(e);
                                       });
                                     },
-                                    icon: Icon(Icons.add, color: Colors.green),
+                                    style: ButtonStyle(
+                                        backgroundColor:
+                                            MaterialStateProperty.all(
+                                                Colors.red)),
+                                    child: Text("Remove To\n Cart"),
                                   ),
                                 ],
                               ),
-                              SizedBox(
-                                height: 5,
-                              ),
-                              ElevatedButton(
-                                onPressed: () {},
-                                child: Text("Add To Cart"),
-                              ),
-                              ElevatedButton(
-                                onPressed: () {
-                                  setState(() {
-                                    cartItem.remove(e);
-                                  });
-                                },
-                                style: ButtonStyle(
-                                    backgroundColor:
-                                        MaterialStateProperty.all(Colors.red)),
-                                child: Text("Remove To\n Cart"),
-                              ),
-                            ],
-                          ),
-                        )
-                      ],
-                    )),
-              ))
+                            )
+                          ],
+                        )),
+                  ))
               .toList(),
-        ),
+          Divider(
+            thickness: 2,
+          ),
+          Text("Total Product - ${cartItem.length}\n",style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold,color: Colors.blue),),
+          Text("Total Amount - $totalAmount",style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold,color: Colors.blue),),
+
+          Divider(
+            thickness: 2,
+          )
+        ]),
       ),
     );
   }
